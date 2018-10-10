eb930be65c776a4966926bbf6268a6df843ae34e
smertelny <smertelny@gmail.com>
2018-10-10 21:20:09 +0300

    Change index page to fetch

--- src/Controller/DataController.php
+++ src/Controller/DataController.php
@@ -8,32 +8,36 @@ use Symfony\Component\Routing\Annotation\Route;
 
 use App\Entity\Log;
 
-function get_data($url) {
-    $handle = curl_init();
-    curl_setopt($handle, CURLOPT_URL, $url);
-    curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-    $result = curl_exec($handle);
-    $status_code = curl_getinfo($handle, CURLINFO_RESPONSE_CODE);
-    if ($status_code === 200) {
-        curl_close($handle);
-        $result = json_decode($result, true);
-        return array(
-            'result' => $result,
-            'status_code' => $status_code
-        );
-    } else {
-        curl_close($handle);
-        return array(
-            'error' => $result,
-            'status_code' => $status_code,
-        );
+
+
+class DataController extends AbstractController 
+{
+    private function get_data($url) 
+    {
+        $handle = curl_init();
+        curl_setopt($handle, CURLOPT_URL, $url);
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($handle);
+        $status_code = curl_getinfo($handle, CURLINFO_RESPONSE_CODE);
+        if ($status_code === 200) 
+        {
+            curl_close($handle);
+            $result = json_decode($result, true);
+            return array(
+                'result' => $result,
+                'status_code' => $status_code
+            );
+        } 
+        else 
+        {
+            curl_close($handle);
+            return array(
+                'error' => $result,
+                'status_code' => $status_code,
+            );
+        }
     }
-}
-
-
-
-class DataController extends AbstractController {
-    function save_logs($ip, $status_code) {
+    private function save_logs($ip, $status_code) {
         $manager = $this->getDoctrine()->getManager();
 
         $log = new Log();
@@ -44,40 +48,53 @@ class DataController extends AbstractController {
         $manager->flush();
     }
 
-    function getRealIpAddr($server) {
-        if (!empty($server->get('HTTP_CLIENT_IP'))) {
+    private function getRealIpAddr($server) {
+        if (!empty($server->get('HTTP_CLIENT_IP'))) 
+        {
             $ip = $server->get('HTTP_CLIENT_IP');
-        } elseif (!empty($server->get('HTTP_X_FORWARDED_FOR'))) {
+        }
+        elseif (!empty($server->get('HTTP_X_FORWARDED_FOR')))
+        {
             $ip = $server->get('HTTP_X_FORWARDED_FOR');
-        } else {
+        } 
+        else 
+        {
             $ip = $server->get('REMOTE_ADDR');
         }
         return $ip;
     }
 
+    // /**
+    //  * @Route("", name="index")
+    //  */
+    // public function index() {
+    //     return $this->
+    // }
+
     /**
-     * @Route("", name="index")
+     * @Route("fetch", name="fetch")
      */
-    function index(Request $request) {
+    public function fetcher(Request $request) 
+    {
         $url = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json';
         if ($request->query->get('date')) {
             $url = $url . "&date=" . $request->query->get('date');
         }
 
-        $result = get_data($url);
+        $result = $this->get_data($url);
         $status_code = $result['status_code'];
         unset($result['status_code']);
         $ip = $this->getRealIpAddr($request->server);
         $this->save_logs($ip, $status_code);
 
-        $yesturday_data = get_data($url . "&date=". date('Ymd', strtotime("-1 day")));
+        $yesturday_data = $this->get_data($url . "&date=". date('Ymd', strtotime("-1 day")));
         $status_code = $yesturday_data['status_code'];
         unset($yesturday_data['status_code']);
         $this->save_logs($ip, $status_code);
 
 
         return $this->render(
-            'app/index.html.twig',
+            'app/fetch.html.twig',
             array(
                 "data" => $result,
                 "yesturday_data" => $yesturday_data,
